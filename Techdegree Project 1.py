ee2c7649f04ac06d99ee82838fbe5c9124eb534f
zinia09 <110440881+zinia09@users.noreply.github.com>
2022-12-21 21:53:19 -0500

    Update Techdegree Project 1.py

--- Techdegree Project 1.py
+++ Techdegree Project 1.py
@@ -8,18 +8,21 @@ def start_game():
     num = random.randint(1, 10)
     attempts = 0
     while True:
-        guess = int(input("Enter your guess: "))
-        attempts += 1
-        if guess == num:
-            print(f"Got it! the number was {num}, your attempt count is {attempts}")
-            print("Thanks for playing :) ")
-            break
-        elif guess not in range(1,11):
-            print('Choose a number between 1 and 10')
-        elif guess < num:
-            print("It's higher ")
-        elif guess > num:
-            print("It's lower ")
+        try:
+            guess = int(input("Enter your guess: "))
+            attempts += 1
+            if guess == num:
+                print(f"Got it! the number was {num}, you had {attempts} attempts.")
+                print("Thanks for playing :) ")
+                break
+            elif guess not in range(1, 11):
+                print('Choose a number between 1 and 10')
+            elif guess < num:
+                print("It's higher ")
+            elif guess > num:
+                print("It's lower ")
+        except:
+            print("enter a value between 1 and 10")
 
 
 start_game()
